1ff07b789030c3834cb633f616d48decefa77d28
gbart0198 <gbart0198@gmail.com>
2025-12-22 22:37:20 -0500

    forgot to add these

--- router/router.go
+++ router/router.go
@@ -17,7 +17,7 @@ func SetupRouter(h *handlers.Handler, frontendURL string, jwtService *auth.JWTSe
 
 	// CORS configuration for HTTP-only cookies
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.1.130:5173", "http://192.168.1.21:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.1.130:5173", "http://192.168.1.21:5173", "http://192.168.1.137:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		AllowCredentials: true, // Required for cookies
